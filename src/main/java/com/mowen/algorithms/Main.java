5dd601c18b38d86e1ef3dd6727474bdc52534003
wenlong.mo <1245146613@qq.com>
2016-04-12 01:51:40 +0800

    比重胜出算法

--- src/main/java/com/mowen/algorithms/Main.java
+++ src/main/java/com/mowen/algorithms/Main.java
@@ -1,10 +1,34 @@
 package com.mowen.algorithms;
 
+import java.util.Random;
+
 /**
  * Created by mowen on 15-11-7.
+ *  比重胜出算法
  */
 public class Main {
+    private static final int LOOPS = 10000;
+    private static final int DEVIACTION = 100;
     public static void main(String[] args) {
-        System.out.println("Learning Algorithms");
+        int[] winner_numbers = new int[3];
+        for(int i =0; i < LOOPS; i++) {
+            winner_numbers[weightedChoice(1, 3, 6)] += 1;
+        }
+
+        assert (winner_numbers[0] > LOOPS * 1/10 - DEVIACTION) && (winner_numbers[0] < LOOPS * 1/10 + DEVIACTION);
+        assert (winner_numbers[1] > LOOPS * 3/10 - DEVIACTION) && (winner_numbers[1] < LOOPS * 3/10 + DEVIACTION);
+        assert (winner_numbers[2] > LOOPS * 6/10 - DEVIACTION) && (winner_numbers[2] <  LOOPS * 6/10 + DEVIACTION);
+    }
+
+    private static int weightedChoice(int ... weights) {
+        int winner = -1;
+        double sum = 0;
+        for(int i = 0; i < weights.length; i++) {
+            sum += weights[i];
+            if(sum * new Random().nextDouble() < weights[i]) {
+                winner = i;
+            }
+        }
+        return winner;
     }
 }
